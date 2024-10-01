dc92e20664d44e18ba96e9341b6af2634947a1bd
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-10-01 18:48:02 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000, () => console.log("App is running!"));
