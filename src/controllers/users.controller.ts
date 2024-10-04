7a23e5903129bc02090448afa75eca4c1a61804b
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-10-04 00:34:47 -0300

    add CRUD firebase

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,21 +3,25 @@ import { getFirestore } from "firebase-admin/firestore";
 
 type User = { id: number; nome: string; email: string };
 
-let usuarios: User[] = [];
-
 class UserController {
-  static getAll(req: Request, res: Response) {
-    res.send(usuarios);
+  static async getAll(req: Request, res: Response) {
+    const snapshot = await getFirestore().collection("users").get();
+    const users = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    res.send(users);
   }
 
-  static getById(req: Request, res: Response) {
-    let userId = Number(req.params.id);
-    let user = usuarios.find((user) => (user.id = userId));
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).send({ message: "Usuário não encontrado" });
-    }
+  static async getById(req: Request, res: Response) {
+    let userId = req.params.id;
+    const doc = await getFirestore().collection("users").doc(userId).get();
+    res.send({
+      id: doc.id,
+      ...doc.data(),
+    });
   }
 
   static async save(req: Request, res: Response) {
@@ -29,21 +33,24 @@ class UserController {
   }
 
   static update(req: Request, res: Response) {
-    let userId = Number(req.params.id);
-    let user = req.body;
-    let indexOF = usuarios.findIndex((_user: User) => _user.id === userId);
-    usuarios[indexOF].nome = user.nome;
-    usuarios[indexOF].email = user.email;
+    let userId = req.params.id;
+    let user = req.body as User;
+
+    getFirestore().collection("users").doc(userId).set({
+      nome: user.nome,
+      email: user.email,
+    });
 
     res.send({
       message: "Usuario aletrado com sucesso!",
     });
   }
 
-  static delete(req: Request, res: Response) {
-    let userId = Number(req.params.id);
-    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
-    usuarios.splice(indexOf, 1);
+  static async delete(req: Request, res: Response) {
+    let userId = req.params.id;
+
+    await getFirestore().collection("users").doc(userId).delete();
+
     res.send({
       message: "Usuario excluido com sucesso!",
     });
